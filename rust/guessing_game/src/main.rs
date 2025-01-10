eeb7b008a0822a295617615fd25e358ea3989d13
Lenghak Hok <lenghakhok.dev@gmail.com>
2025-01-10 17:44:08 +0700

    feat(rust): init guessing game

---
+++ rust/guessing_game/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::{self, Write};
+
+fn main() {
+    println!("Guess the number!");
+    print!("Please input your guess: ");
+    let _ = io::stdout().flush().unwrap();
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
