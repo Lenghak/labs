df2224399d4e69dbd2bd082e930874b417fedd12
Lenghak Hok <lenghakhok.dev@gmail.com>
2025-01-13 11:57:36 +0700

    feat(guessing_game): [rust] added full functionality

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,15 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io::{self, Write};
 
 fn main() {
     println!("Guess the number!");
-    print!("Please input your guess: ");
-    io::stdout().flush().unwrap_or_default();
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        print!("Please input your guess: ");
+        io::stdout().flush().unwrap_or_default();
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Greater => println!("Too big!\n"),
+            Ordering::Equal => {
+                println!("You win!\n");
+                break;
+            }
+        }
+    }
 }
