94d25f104e7fd8937b6233794fdf47a3a5e16cb5
Lenghak <lenghakhok.dev@gmail.com>
2025-09-21 14:33:34 +0700

    feat(rust): added guessing game

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,4 +1,42 @@
+use std::{
+    cmp::Ordering,
+    io::{self, Write},
+};
+
+use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        print!("Please input your guess: ");
+
+        io::stdout().flush().unwrap();
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!();
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("You guessed: {guess} \n");
+    }
 }
