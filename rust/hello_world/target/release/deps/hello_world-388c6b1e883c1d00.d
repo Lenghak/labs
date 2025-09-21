b03f7b8383a09be32ec92fc06eabe5e6b58b7572
Lenghak <lenghakhok.dev@gmail.com>
2025-09-21 13:21:55 +0700

    feat(rust): added nx projects and workspaces

--- rust/hello_world/target/release/deps/hello_world-388c6b1e883c1d00.d
+++
@@ -1,5 +0,0 @@
-/home/lenghak/Projects/labs/rust/hello_world/target/release/deps/hello_world-388c6b1e883c1d00.d: src/main.rs
-
-/home/lenghak/Projects/labs/rust/hello_world/target/release/deps/hello_world-388c6b1e883c1d00: src/main.rs
-
-src/main.rs:
