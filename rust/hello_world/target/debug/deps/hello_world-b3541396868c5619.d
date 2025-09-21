4200f9812512146d592e818c0985250b43026f07
Lenghak <lenghakhok.dev@gmail.com>
2025-09-21 10:25:04 +0700

    feat(rust): added hello_cargo

---
+++ rust/hello_world/target/debug/deps/hello_world-b3541396868c5619.d
@@ -0,0 +1,5 @@
+/home/lenghak/Projects/labs/rust/hello_world/target/debug/deps/hello_world-b3541396868c5619.d: src/main.rs
+
+/home/lenghak/Projects/labs/rust/hello_world/target/debug/deps/hello_world-b3541396868c5619: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/lenghak/Projects/labs/rust/hello_world/target/debug/hello_world: /home/lenghak/Projects/labs/rust/hello_world/src/main.rs
